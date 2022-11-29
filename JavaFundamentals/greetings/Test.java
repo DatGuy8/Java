93afcc60eac6ca396a88f275c27953b00e902ae6
DatGuy8 <namesjohntran@gmail.com>
2022-11-28 23:23:46 -0800

    first commit

---
+++ JavaFundamentals/greetings/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("My name is John Tran");
+        System.out.println("I am 31 years old");
+        System.out.println("My hometown is Honolulu, Hawaii");
+    }
+    
+    
+}
---
+++ JavaFundamentals/javaFun/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args){
+        Integer ourInt = 5321;
+        
+        double pi = 3;
+        boolean bool = true;
+        char singleCharater = 'A';
+
+        String multipleCharacters = "ABC";
+        String myString = ourInt.toString();
+        System.out.println(ourInt.intValue());
+        System.out.println(myString.length());
+        // System.out.println(singleCharater);
+    }
+}
