63383d17bf0f3187ca2d07060005fe48d832a0fb
DatGuy8 <namesjohntran@gmail.com>
2022-11-30 10:44:51 -0800

    commit

--- JavaFundamentals/CafeBusinessLogic/CafeUtil.java
+++ JavaFundamentals/CafeBusinessLogic/CafeUtil.java
@@ -1,11 +1,60 @@
+import java.util.ArrayList;
+
 public class CafeUtil {
+
 
     public int getStreakGoal() {
         int sum = 0;
-        for(int i = 1; i < 11; i++){
+        for(int i = 1; i <= 10; i++){
             sum += i;
             
         }
         return sum;
+    }
+
+    public int getStreakGoal(int numWeeks) {
+        int sum = 0;
+        for(int i = 1; i <= numWeeks; i++){
+            sum += i;
+            
+        }
+        return sum;
+    }
+
+
+    public double getOrderTotal(double[] prices){
+        double sum = 0;
+        for(double item: prices){
+            sum += item;
+        }
+        return sum;
+    }
+
+    public void displayMenu(ArrayList<String> menu){
+        String coffees;
+        for(int i = 0; i < menu.size(); i++){
+            System.out.println(i + ": " + menu.get(i));
+
+        }
+
+    }
+
+    public void addCustomer(ArrayList<String> customers){
+        System.out.println("Please enter your name:");
+        String userName = System.console().readLine();
+        System.out.println("Hello " + userName +"!");
+        System.out.printf("There are %s people in front of you", customers.size());
+        customers.add(userName);
+
+    }
+
+    public void printPriceChart(String product, double price, int maxQuality){
+        System.out.println(product);
+        double sum = 0;
+        for(int i = 1; i <= maxQuality; i++){
+            sum += price;
+            System.out.printf("%s - $%.2f \n", i , sum);
+        }
+
     }
 }
--- JavaFundamentals/CafeBusinessLogic/TestCafe.java
+++ JavaFundamentals/CafeBusinessLogic/TestCafe.java
@@ -1,8 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TestCafe {
     public static void main(String[] args){
 
         CafeUtil cafeBot = new CafeUtil();
+        
 
-        System.out.println(cafeBot.getStreakGoal());
+        System.out.println("==========Streak GOAL TEST ============");
+        System.out.printf("Purchases needed by week 10: %s \n\n", cafeBot.getStreakGoal());
+
+        System.out.printf("Purchases needed by week %s: %s \n\n" , 56 , cafeBot.getStreakGoal(56));
+        
+
+
+        System.out.println("----- Order Total Test-----");
+        double[] lineItems = {3.5, 1.5, 4.0, 4.5};
+        System.out.printf("Order total: %s \n\n", cafeBot.getOrderTotal(lineItems));
+        
+        System.out.println("----- Display Menu Test-----");
+        ArrayList<String> menu = new ArrayList<String>();
+        menu.add("drip coffee");
+        menu.add("cappuccino");
+        menu.add("latte");
+        menu.add("mocha");
+        menu.add("pumpkin latte");
+        cafeBot.displayMenu(menu);
+    
+
+        System.out.println("\n----- Add Customer Test-----");
+        ArrayList<String> customers = new ArrayList<String>();
+        // --- Test 4 times ---
+        for (int i = 0; i < 4; i++) {
+            cafeBot.addCustomer(customers);
+            System.out.println("\n");
+
+        cafeBot.printPriceChart("Coffee Grounds Blended", 14.5, 4);
+
     }
+}
 }
