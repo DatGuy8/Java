c208917833e19e4a72b41513d52741b6130e6a7f
DatGuy8 <namesjohntran@gmail.com>
2022-11-29 20:28:13 -0800

    fda

---
+++ JavaFundamentals/CafeBusinessLogic/TestCafe.java
@@ -0,0 +1,8 @@
+public class TestCafe {
+    public static void main(String[] args){
+
+        CafeUtil cafeBot = new CafeUtil();
+
+        System.out.println(cafeBot.getStreakGoal());
+    }
+}
--- JavaFundamentals/Test/Test.java
+++ JavaFundamentals/Test/Test.java
@@ -1,26 +1,40 @@
+import java.util.ArrayList;
 
 public class Test {
     
     public static void main(String[] args){
-        // Print "Hello Sam, how are you today?" and "Hello Rick Jones, how are you today?"
-    String user1 = greeting("John", "Tran");
-    System.out.println(user1);
-    Greeting greetApp = new Greeting();
-    
-    String dateMessage = greetApp.getCurrentDate();
-    System.out.println(dateMessage);
-        // Your code here
-        
+
+        ArrayList<Integer> myList = new ArrayList<Integer>();
+
+
+
+        myList.add(10);
+        myList.add(11);
+        myList.add(51);
+
+        System.out.println(myList);
+
+        Integer num = myList.get(0);
+
+        System.out.println(num);
+        System.out.println(myList);
+
+        myList.set(0, 100);
+
+        System.out.println(myList);
+
+
+        ArrayList<Object> things = new ArrayList<Object>();
+        things.add(10);
+        things.add("hello");
+        things.add(new ArrayList<Integer>());
+        things.add(12.4);
+        System.out.println(things);
+        System.out.println(things.remove(12.4));
+        System.out.println(things);
+
     }
 
-    // Don't worry about the 'public' and 'static' keywords for now.
-    // We will discuss them later.
-    public static String greeting(String name){
-        return String.format("Hello %s, how are you today?", name);
-    }
 
-    public static String greeting(String firstName, String lastName){
-        return String.format("Hello %s %s, how are you today?", firstName, lastName);
-    }
 
 }
